054134333a4f20868c0104c4891669d9e6fe520f
Nathanael Lee <nathanael.lee@edu.sait.ca>
2026-03-05 13:33:58 -0700

    finished form input

--- app/(tabs)/employee.tsx
+++ app/(tabs)/employee.tsx
@@ -1,112 +1,136 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Formik } from "formik";
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as Yup from "yup";
 
-import { Collapsible } from '@/components/ui/collapsible';
-import { ExternalLink } from '@/components/external-link';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Fonts } from '@/constants/theme';
+const SignupSchema = Yup.object().shape({
+  fullName: Yup.string().required("Full name is required"),
+  DoB: Yup.date()
+    .required("Date of birth is required")
+    .typeError("Invalid date format, please use YYYY-MM-DD"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  phoneNumber: Yup.string()
+    .matches(/^\d{3}-\d{3}-\d{4}$/, "Please put in the format ###-###-####")
+    .min(12, "please put in the format ###-###-####")
+    .max(12, "please put in the format ###-###-####")
+    .required("Phone number is required use the format ###-###-####"),
+  jobTitle: Yup.string().required("Job title is required"),
+});
+
+const MyForm = () => (
+  <Formik
+    initialValues={{
+      fullName: "",
+      DoB: "",
+      email: "",
+      phoneNumber: "",
+      jobTitle: "",
+    }}
+    validationSchema={SignupSchema}
+    onSubmit={(values) =>
+      Alert.alert(
+        `Name: ${values.fullName}\nDoB: ${values.DoB}`,
+        `Email: ${values.email}\nPhone: ${values.phoneNumber}\nTitle: ${values.jobTitle}`,
+      )
+    }
+  >
+    {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+      <View>
+        <TextInput
+          placeholder="Full Name"
+          onChangeText={handleChange("fullName")}
+          onBlur={handleBlur("fullName")}
+          value={values.fullName}
+          autoCapitalize="words"
+          style={{
+            borderWidth: 1,
+            backgroundColor: "red",
+            marginBottom: 5,
+            padding: 8,
+          }}
+        />
+        {errors.fullName && touched.fullName ? (
+          <Text>{errors.fullName}</Text>
+        ) : null}
+
+        <TextInput
+          placeholder="Date of Birth, put in the format YYYY-MM-DD"
+          onChangeText={handleChange("DoB")}
+          onBlur={handleBlur("DoB")}
+          value={values.DoB}
+          style={{
+            borderWidth: 1,
+            backgroundColor: "red",
+            marginBottom: 5,
+            padding: 8,
+          }}
+        />
+        {errors.DoB && touched.DoB ? <Text>{errors.DoB}</Text> : null}
+
+        <TextInput
+          placeholder="Email"
+          onChangeText={handleChange("email")}
+          onBlur={handleBlur("email")}
+          value={values.email}
+          autoCapitalize="none"
+          style={{
+            borderWidth: 1,
+            backgroundColor: "red",
+            marginBottom: 5,
+            padding: 8,
+          }}
+        />
+        {errors.email && touched.email ? <Text>{errors.email}</Text> : null}
+
+        <TextInput
+          placeholder="Phone Number"
+          onChangeText={handleChange("phoneNumber")}
+          onBlur={handleBlur("phoneNumber")}
+          value={values.phoneNumber}
+          style={{
+            borderWidth: 1,
+            backgroundColor: "red",
+            marginBottom: 5,
+            padding: 8,
+          }}
+        />
+        {errors.phoneNumber && touched.phoneNumber ? (
+          <Text>{errors.phoneNumber}</Text>
+        ) : null}
+
+        <TextInput
+          placeholder="Job Title"
+          onChangeText={handleChange("jobTitle")}
+          onBlur={handleBlur("jobTitle")}
+          value={values.jobTitle}
+          style={{
+            borderWidth: 1,
+            backgroundColor: "red",
+            marginBottom: 5,
+            padding: 8,
+          }}
+        />
+        {errors.jobTitle && touched.jobTitle ? (
+          <Text>{errors.jobTitle}</Text>
+        ) : null}
+
+        <Button onPress={() => handleSubmit()} title="Submit" />
+      </View>
+    )}
+  </Formik>
+);
 
 export default function TabTwoScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText
-          type="title"
-          style={{
-            fontFamily: Fonts.rounded,
-          }}>
-          Explore
-        </ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image
-          source={require('@/assets/images/react-logo.png')}
-          style={{ width: 100, height: 100, alignSelf: 'center' }}
-        />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user&apos;s current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful{' '}
-          <ThemedText type="defaultSemiBold" style={{ fontFamily: Fonts.mono }}>
-            react-native-reanimated
-          </ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+    <SafeAreaView style={styles.bg}>
+      <MyForm />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
+  bg: {
+    backgroundColor: "green",
+    height: "100%",
   },
 });
